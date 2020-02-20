0daaa243b3d20ce1c69918ef908a5758dc0b40e1
Yuriy Belikov <yuriybelikov1@gmail.com>
2020-02-20 13:46:08 +0300

    Move cycle detection vistitor and algorithm to appropriate folder

--- boost-graph/boost-graph/headers/CycleDetectionVisitor.h
+++ boost-graph/boost-graph/headers/CycleDetectionVisitor.h
@@ -1,1 +1,13 @@
 #pragma once
+#include <boost/graph/graph_traits.hpp>
+#include "./headers/DefaultVisitor.h"
+
+template<typename Graph>
+class CycleDetectionVisitor : public DefaultVisitor {
+	CycleDetectionVisitor(bool& cycle) : hasCycle{cycle}{}
+	void backEdge(typename boost::graph_traits<Graph>::edge_descriptor edge, const Graph& g) {
+		hasCycle = true;
+	}
+private:
+	bool& hasCycle;
+};
--- boost-graph/boost-graph/headers/algorithms/CycleDetection.h
+++ boost-graph/boost-graph/headers/algorithms/CycleDetection.h
@@ -1,1 +1,10 @@
 #pragma once
+#include "./headers/CycleDetectionVisitor.h"
+#include "./headers/algorithms/TopoSort.h"
+
+template<typename Graph>
+bool hasCycle(const Graph& g) {
+	bool hasCycle = false;
+	CycleDetectionVisitor vis(hasCycle);
+	genericDFS(g, vis);
+}
